2c9fe4332f785793b0bc91880eed3653a4f588ff
Kawona <ojekaona@gmail.com>
2025-10-03 08:08:13 +0000

    initial commit for my project

---
+++ alx-project-0x04/pages/text-to-image.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function TextToImage() {
+  return (
+    <Layout>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Text to Image Page</h1>
+      </div>
+    </Layout>
+  );
+}
